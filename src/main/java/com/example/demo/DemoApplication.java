6e881f13d4e1a7f536bcd68abe2400fc3897ba0c
Andres Bonilla <andres94b@gmail.com>
2025-08-05 14:19:08 -0400

    Added JPA

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.dao.StudentRepository;
+import com.example.demo.model.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,11 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+		return args -> {
+			Student maria = new Student("Maria", "Jones", "maria.jones@gmail.com",21);
+			studentRepository.save(maria);
+		};
+	}
 }
